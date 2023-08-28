18efc5b4254d6ec1c67760ef1908a8df6fa27c39
ChristianHinson <hinsonc9403@student.faytechcc.edu>
2023-08-28 11:25:45 -0400

    M1Lab

---
+++ M1LabHinson/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+CSC 134
+M1LAB - APPLE ORCHARD
+Hinson
+8/28/2023
+simple interactive store
+*/
+using namespace std;
+
+int main()
+{
+    string name ="Hinson ";
+    cout << "welcome to the " << name << "Orchard" << endl;
+    return 0;
+}
